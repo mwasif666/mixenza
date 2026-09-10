b060b962310eaacaa69e4c5964ebe0a624c245be
Muhammad Wasif <115789986+mwasif666@users.noreply.github.com>
2026-09-10 18:53:19 +0500

    Route TheOnlineStore products to live Mixenza detail pages

--- src/app/product/default/page.tsx
+++ src/app/product/default/page.tsx
@@ -1,29 +1,44 @@
-'use client'
-import React, { useState } from 'react'
-import { useSearchParams } from 'next/navigation';
-import Link from 'next/link'
+import React from 'react'
 import TopNavOne from '@/components/Header/TopNav/TopNavOne'
 import MenuOne from '@/components/Header/Menu/MenuOne'
 import BreadcrumbProduct from '@/components/Breadcrumb/BreadcrumbProduct'
-import Default from '@/components/Product/Detail/Default';
+import Default from '@/components/Product/Detail/Default'
+import SourceProductDetail from '@/components/TheOnlineStore/SourceProductDetail'
 import Footer from '@/components/Footer/Footer'
-import { ProductType } from '@/type/ProductType'
 import productData from '@/data/Product.json'
+import { getSourceProductById } from '@/lib/theOnlineStore'
 
-const ProductDefault = () => {
-    const searchParams = useSearchParams()
-    let productId = searchParams.get('id')
+interface Props {
+    searchParams: { id?: string }
+}
 
-    if (productId === null) {
-        productId = '1'
+export const revalidate = 900
+
+const ProductDefault = async ({ searchParams }: Props) => {
+    const productId = searchParams.id || '1'
+
+    if (productId.startsWith('source-')) {
+        const sourceProduct = await getSourceProductById(productId.replace(/^source-/, ''))
+        if (sourceProduct) {
+            return (
+                <>
+                    <TopNavOne props="style-one bg-black" slogan="New customers save 10% with the code GET10" />
+                    <div id="header" className="relative w-full">
+                        <MenuOne props="bg-white" />
+                    </div>
+                    <SourceProductDetail product={sourceProduct} />
+                    <Footer />
+                </>
+            )
+        }
     }
 
     return (
         <>
             <TopNavOne props="style-one bg-black" slogan="New customers save 10% with the code GET10" />
-            <div id="header" className='relative w-full'>
+            <div id="header" className="relative w-full">
                 <MenuOne props="bg-white" />
-                <BreadcrumbProduct data={productData} productPage='default' productId={productId} />
+                <BreadcrumbProduct data={productData} productPage="default" productId={productId} />
             </div>
             <Default data={productData} productId={productId} />
             <Footer />
