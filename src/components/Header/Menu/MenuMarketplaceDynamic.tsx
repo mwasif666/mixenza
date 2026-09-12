f5a42e3016e614509ef506d19ad97e078900750b
Muhammad Wasif <115789986+mwasif666@users.noreply.github.com>
2026-09-12 15:02:19 +0500

    feat: add dynamic marketplace menu wrapper

---
+++ src/components/Header/Menu/MenuMarketplaceDynamic.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import MenuMarketplace from './MenuMarketplace'
+import DynamicMarketplaceMenu from '@/components/TheOnlineStore/DynamicMarketplaceMenu'
+
+export default function MenuMarketplaceDynamic() {
+    return (
+        <>
+            <MenuMarketplace />
+            <DynamicMarketplaceMenu />
+        </>
+    )
+}
